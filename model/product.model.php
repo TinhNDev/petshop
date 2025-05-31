92cefc2da1395bd0b18e57717606687e20b633a2
tinhnt <nttinh102003@gmail.com>
2025-05-31 16:40:51 +0700

    --no-edit

--- model/product.model.php
+++ model/product.model.php
@@ -95,13 +95,10 @@ class Product
         return false;
     }
 
-    public function findAll($page = 1, $limit = 10)
+    public function findAll()
     {
-        $offset = ($page - 1) * $limit;
-        $query = "SELECT * FROM {$this->table_name} LIMIT :limit OFFSET :offset";
+        $query = "SELECT * FROM {$this->table_name}";
         $stmt = $this->conn->prepare($query);
-        $stmt->bindValue(':limit', (int) $limit, PDO::PARAM_INT);
-        $stmt->bindValue(':offset', (int) $offset, PDO::PARAM_INT);
         $stmt->execute();
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
